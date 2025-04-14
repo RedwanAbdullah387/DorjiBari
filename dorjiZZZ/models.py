17b8263ec7db9e648e45eb63ad69c9c3c7b3409f
RedwanAbdullah387 <blackmagnum387@gmail.com>
2025-04-14 20:44:10 +0600

    update before presentation

--- dorjiZZZ/models.py
+++ dorjiZZZ/models.py
@@ -1,97 +1,106 @@
 from django.db import models
-
 from django.contrib.auth.models import User
-
 from django.core.validators import MinValueValidator, MaxValueValidator
-
 
 # Create your models here.
 
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+class UserProfile(models.Model):
+ NID_LENGTH = 5
+ user = models.OneToOneField(User, on_delete=models.CASCADE)
+ name = models.CharField(max_length=100)
+ # email = models.EmailField(blank=True, null=True)
+ nid = models.CharField(max_length=NID_LENGTH,default='12345')  # min length 10
+ contact_no = models.CharField(max_length=20, blank=True, null=True, default='+880')
 
-    user_mobile = models.CharField(max_length=11, null=True, blank=True)
+ picture = models.ImageField(upload_to='images/', blank=True, null=True,
+                             default='images/Default_pic.jpg')
+ about_myself = models.TextField(blank=True, null=True, default='will add later')
+ address = models.CharField(max_length=255)
 
-    user_profile_pic = models.ImageField(upload_to='upload', null=True, blank=True, default='../upload/Default_pic.png')
+ def __str__(self):
+  return self.name
 
-    # need to include default_pic
-
-    user_type = (
-
-        ('normal', 'normal'),
-
-        ('designer', 'designer')
-
-    )
-
-    type_of_user = models.CharField(max_length=10, choices=user_type, blank=True, null=True)
-
-    def __str__(self):
-        return self.user.username
 
 
 class Items(models.Model):
-    item_name = models.CharField(max_length=200)
+ id = models.IntegerField(primary_key=True)
+ item_name = models.CharField(max_length=200)
+ item_description = models.TextField(blank=True, null=True)
+ item_pic = models.ImageField(upload_to='images/', null=True, blank=True,default='images/Default_pic.jpg')
+ item_price = models.FloatField()
+ item_category = models.CharField(max_length=200)
+ item_stock = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
 
-    item_description = models.TextField(blank=True, null=True)
-
-    item_pic = models.ImageField(upload_to='upload', null=True, blank=True)
-
-    item_price = models.FloatField()
-
-    item_category = models.CharField(max_length=200)
-
-    item_stock = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(50)])
-
-    def __str__(self):
-        return self.item_name
+ def __str__(self):
+    return self.item_name
 
 
-class Product(models.Model):
-    product_name = models.CharField(max_length=200)
+class ProductWomen(models.Model):
+ id = models.IntegerField(primary_key=True)
+ product_name = models.CharField(max_length=200)
+ product_description = models.TextField(blank=True, null=True)
+ product_pic = models.ImageField(upload_to='images/', null=True, blank=True,default='images/Default_pic.jpg')
+ product_price = models.FloatField()
+ product_category = models.CharField(max_length=200)
+ product_stock = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(50)])
 
-    product_description = models.TextField(blank=True, null=True)
 
-    product_pic = models.ImageField(upload_to='upload', null=True, blank=True)
+ def __str__(self):
+    return self.product_name
 
-    product_price = models.FloatField()
 
-    product_category = models.CharField(max_length=200)
-
-    product_stock = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(50)])
-
-    product_type = (
-
-        ('Men', 'Men'),
-
-        ('Women', 'Women')
-
-    )
-
-    type_of_product = models.CharField(max_length=10, choices=product_type, blank=True, null=True)
-
-    def __str__(self):
-        return self.product_name
+class Men(models.Model):
+ id = models.IntegerField(primary_key=True)
+ product_name = models.CharField(max_length=200)
+ product_description = models.TextField(blank=True, null=True)
+ product_pic = models.ImageField(upload_to='images/', null=True, blank=True,default='images/Default_pic.jpg')
+ product_price = models.FloatField()
+ product_category = models.CharField(max_length=200)
+ product_stock = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(50)])
+ def __str__(self):
+    return self.product_name
 
 
 class Design(models.Model):
-    design_name = models.CharField(max_length=200)
+ design_name = models.CharField(max_length=200)
+ design_description = models.TextField(blank=True, null=True)
+ design_pic = models.ImageField(upload_to='images/', null=True, blank=True,default='images/Default_pic.png')
+ design_price = models.FloatField()
 
-    design_description = models.TextField(blank=True, null=True)
+ def __str__(self):
+    return self.design_name
 
-    design_pic = models.ImageField(upload_to='upload', null=True, blank=True)
-
-    design_price = models.FloatField()
-
-    def __str__(self):
-        return self.design_name
 
 class Cart(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+ user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+ product = models.ForeignKey(Items, on_delete=models.CASCADE, blank=True, null=True)
+ quantity = models.PositiveIntegerField(default=1, blank=True, null=True)
 
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, blank=True, null=True)
+ def __str__(self):
+  return f"Cart for {self.user}"
 
-    create_date = models.DateTimeField(auto_now_add=True, auto_now=False)
 
-    update_date = models.DateTimeField(auto_now_add=True, auto_now=False)
+class Order(models.Model):
+
+
+ PAYMENT_CHOICES = [
+
+  ('bkash', 'bkash'),
+  ('bank', 'bank'),
+  ('nagad', 'nagad'),
+  ('COD', 'COD'),
+  ('rocket', 'rocket'),
+
+ ]
+
+ user = models.ForeignKey(User, on_delete=models.CASCADE)
+ product = models.ForeignKey(Items, on_delete=models.CASCADE, blank=True, null=True)
+ p_quantity = models.PositiveIntegerField(default=1, blank=True, null=True)
+
+ shipping_address = models.TextField(max_length=200)
+ payment_method = models.CharField(max_length=30, choices=PAYMENT_CHOICES, default='COD', blank=True, null=True)
+
+
+ def __str__(self):
+  return f"Order {self.id} by {self.user}"
--- smsystem/urls.py
+++ smsystem/urls.py
@@ -16,14 +16,38 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+
+from django.conf.urls.static import static
+from django.conf import settings
+
 from dorjiZZZ import views as j_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', j_view.home, name='home'),
-    path('women/', j_view.women, name='women'),
-    path('men/', j_view.men, name='men'),
-    path('designer/', j_view.designer, name='designer'),
-    path('cart/', j_view.cart, name='cart'),
-    path('contact/', j_view.contact, name='contact'),
+    path('',j_view.home,  name='home'),
+    path('<str:pk>',j_view.itemDetails,name ='itemDetails'),
+    path('createItem/',j_view.createItem,name='createItem'),
+    path('updateItem/<str:pk>',j_view.updateItem,name='updateItem'),
+    path('deleteItem/<str:pk>',j_view.deleteItem,name='deleteItem'),
+    path('women/',j_view.women, name='women'),
+    path('women/<str:pk>/',j_view.womenDetails,name ='womenDetails'),
+    path('men/',j_view.men, name='men'),
+    path('men/<str:pk>/',j_view.MenDetails,name ='MenDetails'),
+    path('designer/',j_view.designer, name='designer'),
+    path('cart/',j_view.cart, name='cart'),
+    path('add_to_cart/<int:product_id>/', j_view.add_to_cart, name='add_to_cart'),
+    path('remove_from_cart/<int:item_id>/', j_view.remove_from_cart, name='remove_from_cart'),
+    path('orders/', j_view.orders, name='orders'),
+    path('orderForm/', j_view.add_orders, name='orderForm'),
+    path('contact/',j_view.contact,name='contact'),
+    path('registration/',j_view.Registration,name='Registration'),
+    path('login/',j_view.loginPage,name='loginPage'),
+    path('logout/', j_view.logoutuser, name='logoutuser'),
+    path('user/',j_view.userPage,name='userPage'),
+    path('userProfile/',j_view.userProfile,name='userProfile'),
+
+    path('profileForm/', j_view.userProfile_add, name='profileForm'),
+    path('profileForm/edit_profile/', j_view.edit_profile, name='edit_profile')
+
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
